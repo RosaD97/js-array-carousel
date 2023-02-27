3665bbdb211f946d328e075d269a0a2ef84263f8
RosaD97 <r960595@gmail.com>
2023-02-27 17:17:31 +0100

    frecce funzionanti

--- js/main.js
+++ js/main.js
@@ -5,15 +5,38 @@ const container = document.querySelector('.ms_container');
 
 let items = '';
 
-for( let i = 0; i < imgs.length; i++){
-    console.log(imgs[i]);
+// Posizione di partenza
+let active = 0;
 
-    items += '<img class="ms_img" src="img/01.webp">'
+for( let i = 0; i < imgs.length; i++){
+    // Creazione struttura in html
+    items += `<img class="ms_img" src="img/${imgs[i]}">` 
 
 }
 
 container.innerHTML += items;
 document.querySelector('.ms_img').classList.add('show');
 
+// Frecce
 const up = document.querySelector('.fa-circle-chevron-up');
 const down = document.querySelector('.fa-circle-chevron-down');
+const item = document.querySelectorAll('.ms_img');
+
+// Evento su frecce
+up.addEventListener('click', 
+function(){
+
+    if (active < item.length - 1){
+        item[active].classList.remove('show');
+        active++;
+        item[active].classList.add('show');
+    }
+})
+down.addEventListener('click', 
+function(){
+    if (active > 0){
+        item[active].classList.remove('show');
+        active--;
+        item[active].classList.add('show');
+    }
+})
