09302d1b86c4c77f2ce2c07a7fc3640161e09a3c
RosaD97 <r960595@gmail.com>
2023-02-27 16:23:47 +0100

    Step 2 fatto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const imgs = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
+const container = document.querySelector('.ms_container');
+
+let items = '';
+
+for( let i = 0; i < imgs.length; i++){
+    console.log(imgs[i]);
+
+    items += '<img class="ms_img" src="img/01.webp">'
+
+}
+
+container.innerHTML += items;
+document.querySelector('.ms_img').classList.add('show');
+
+const up = document.querySelector('.fa-circle-chevron-up');
+const down = document.querySelector('.fa-circle-chevron-down');
